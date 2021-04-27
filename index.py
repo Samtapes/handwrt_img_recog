931e8042f05053b32ec7e650fcd6a7363dc6b5c8
Gustavo Lima <samtapes48@gmail.com>
2021-04-27 15:17:55 -0300

    Model changed to WideResNet 22

--- index.py
+++ index.py
@@ -2,85 +2,100 @@ import torch
 import torchvision
 from torchvision.datasets import MNIST
 
-from torchvision.transforms import ToTensor
+import torchvision.transforms as tt
+
+from torch.utils.data import DataLoader
+from torch import nn, optim
+import torch.nn.functional as F
 
 import numpy as np
 
-from torch.utils.data import SubsetRandomSampler
-from torch.utils.data import DataLoader
 
-import torch.nn as nn
-
-import torch.nn.functional as F
-
-
-## IMPORTING DATASET
-dataset = MNIST('image_cla_lr/data', train=True, transform=ToTensor())
-evaluate_dataset = MNIST('image_cla_lr/data', train=False, transform=ToTensor())
-
-
-
-
-## SPLITING INDICES
-def split_indices(n, val_pct, seed=99):
-  n_val = int(n * val_pct)
-
-  np.random.seed(seed)
-
-  idxs = np.random.permutation(n)
-
-  return idxs[n_val:], idxs[:n_val]
-
-test_indices, val_indices = split_indices(len(evaluate_dataset), 0.9, 42)
-
+## IMPORTING DATA
+dataset = MNIST('image_cla_lr/data', train=True, transform=tt.Compose([tt.ToTensor(), tt.Normalize(mean=(0.5), std=(0.5))]))
+val_dataset = MNIST('image_cla_lr/data', train=False, transform=tt.Compose([tt.ToTensor(), tt.Normalize(mean=(0.5), std=(0.5))]))
 
 
 
 ## CREATING DATALOADERS
-batch_size = 100
+batch_size = 256
 
 train_dl = DataLoader(dataset, batch_size, shuffle=True)
-
-
-val_sampler = SubsetRandomSampler(val_indices)
-val_dl = DataLoader(evaluate_dataset, batch_size, sampler=val_sampler)
-
-
-test_sampler = SubsetRandomSampler(test_indices)
-test_dl = DataLoader(evaluate_dataset, batch_size, sampler=test_sampler)
+val_dl = DataLoader(val_dataset, batch_size, shuffle=True)
 
 
 
+## CREATING THE MODEL
+def conv_2d(ni, nf, ks=3, stride=1):
+  return nn.Conv2d(ni, nf, kernel_size=ks, stride=stride, padding=ks//2, bias=False)
 
-## MODEL
-model = nn.Sequential(
-  nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
-  nn.ReLU(),
-  nn.AvgPool2d(2,2), # bc, 16, 14, 14 
+def bn_relu_conv(ni, nf):
+  return nn.Sequential(
+    nn.BatchNorm2d(ni),
+    nn.ReLU(inplace=True),
+    conv_2d(ni, nf)
+  )
 
-  nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-  nn.ReLU(),
-  nn.AvgPool2d(2,2), # bc, 32, 7, 7
+class ResidualBlock(nn.Module):
+  def __init__(self, ni, nf, stride=1):
+    super(ResidualBlock, self).__init__()
 
-  nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-  nn.ReLU(),
-  nn.AvgPool2d(2,2), # bc, 64, 3.5, 3.5
+    self.bn = nn.BatchNorm2d(ni)
+    self.conv1 = conv_2d(ni, nf, stride=stride)
+    self.conv2 = bn_relu_conv(nf, nf)
+    self.shortcut = lambda x: x
+    if ni != nf:
+      self.shortcut = conv_2d(ni, nf, ks=1, stride=stride)
+    
+  def forward(self, x):
+    x = F.relu(self.bn(x), inplace=True)
+    r = self.shortcut(x)
+    x = self.conv1(x)
+    x = self.conv2(x) * 0.2
+    return x.add_(r)
 
-  nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-  nn.ReLU(),
-  nn.AvgPool2d(2,2),
+def make_group(N, ni, nf, stride):
+  start = ResidualBlock(ni, nf, stride)
+  rest = [ResidualBlock(nf, nf) for j in range(1, N)]
+  return [start] + rest
 
-  nn.Flatten(),
-  nn.Linear(128, 10)
-)
 
+class WideResNet(nn.Module):
+  def __init__(self, start_channel, N, n_classes, groups=3, k=6):
+    super().__init__()
+
+    layers = [conv_2d(1, start_channel)]
+    channels = [start_channel]
+
+    for i in range(groups):
+      channels.append(start_channel*(2**i)*k)
+      stride = 2 if i > 0 else 1
+      layers += make_group(N, channels[i], channels[i+1], stride)
+
+    layers += [
+      nn.BatchNorm2d(channels[-1]),
+      nn.ReLU(inplace=True),
+      nn.AdaptiveAvgPool2d(1),
+      nn.Flatten(),
+      nn.Linear(channels[-1], n_classes)
+    ]
+
+    self.features = nn.Sequential(*layers)
+
+  def forward(self, x): return self.features(x)
+
+
+def wdr22():
+  return WideResNet(start_channel=16, N=3, n_classes=10, groups=3, k=6)
+
+model = wdr22()
 
 
 
 ## TRAINING THE MODEL
 def loss_batch(model, loss_fn, xb, yb, opt=None, metric=None):
-  pred = model(xb)
-  loss = loss_fn(pred, yb)
+  out = model(xb)
+  loss = loss_fn(out, yb)
 
   if opt is not None:
     loss.backward()
@@ -89,8 +104,8 @@ def loss_batch(model, loss_fn, xb, yb, opt=None, metric=None):
 
   metric_result = None
   if metric is not None:
-    metric_result = metric(pred, yb)
-
+    metric_result = metric(out, yb)
+  
   return loss.item(), len(xb), metric_result
 
 
@@ -102,9 +117,7 @@ def accuracy(out, yb):
 def evaluate(model, loss_fn, val_dl, metric=None):
   with torch.no_grad():
     results = [loss_batch(model, loss_fn, xb, yb, metric=metric) for xb, yb in val_dl]
-
     losses, nums, metrics = zip(*results)
-
     total = np.sum(nums)
 
     avg_loss = np.sum(np.multiply(losses, nums)) / total
@@ -112,35 +125,32 @@ def evaluate(model, loss_fn, val_dl, metric=None):
     avg_metric = None
     if metric is not None:
       avg_metric = np.sum(np.multiply(metrics, nums)) / total
-    
+  
   return avg_loss, total, avg_metric
-
 
 
 def fit(epochs, model, loss_fn, train_dl, val_dl, lr, opt_fn=None, metric=None):
 
-  if opt_fn is None: opt_fn = torch.optim.SGD
-  opt = opt_fn(model.parameters(), lr=lr)
+  if opt_fn is None: opt_fn = optim.Adam
+  opt = opt_fn(model.parameters(), lr)
 
   train_losses, val_losses, val_metrics = [], [], []
 
   for epoch in range(epochs):
-    print("NEW EPOCH:", epoch + 1, "/", epochs)
+    print("NEW EPOCH:", epoch+1, "/", epochs)
     model.train()
     for xb, yb in train_dl:
-      train_loss, _, _ = loss_batch(model, loss_fn, xb, yb, opt)
-    
-    # EVALUTING
-    model.eval()
-    avg_loss, _, avg_metric = evaluate(model, loss_fn, val_dl, metric)
-    print(avg_loss, avg_metric)
+      train_loss, _, _ = loss_batch(model, loss_fn, xb, yb, opt=opt)
 
+    model.eval()
+    val_loss, _, val_metric = evaluate(model, loss_fn, val_dl, metric=metric)
+    print(train_loss, val_loss, val_metric)
+    
     train_losses.append(train_loss)
-    val_losses.append(avg_loss)
-    val_metrics.append(avg_metric)
+    val_losses.append(val_loss)
+    val_metrics.append(val_metric)
   
   return train_losses, val_losses, val_metrics
-
 
 
 
@@ -151,16 +161,13 @@ def get_default_device():
   else:
     return torch.device('cpu')
 
-
 device = get_default_device()
-
 
 
 def to_device(data, device):
   if isinstance(data, (list, tuple)):
     return [to_device(x, device) for x in data]
   return data.to(device, non_blocking=True)
-
 
 
 class DeviceDataLoader():
@@ -177,16 +184,14 @@ class DeviceDataLoader():
 
 train_dl = DeviceDataLoader(train_dl, device)
 val_dl = DeviceDataLoader(val_dl, device)
-test_dl = DeviceDataLoader(test_dl, device)
-
 model.to(device)
 
-
-
 loss_fn = F.cross_entropy
+opt_fn = optim.Adam
 
-opt_fn = torch.optim.SGD
 
-fit(20, model, loss_fn, train_dl, val_dl, 0.05, opt_fn, accuracy)
 
-torch.save(model.state_dict(), 'image_cla_lr/models/model6')
+fit(2, model, loss_fn, train_dl, val_dl, 5e-3, opt_fn, accuracy)
+fit(2, model, loss_fn, train_dl, val_dl, 5e-4, opt_fn, accuracy)
+
+torch.save(model.state_dict(), 'image_cla_lr/models/model8')
--- run.py
+++ run.py
@@ -12,6 +12,8 @@ from torch.utils.data import SubsetRandomSampler
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
 
+import torch.nn.functional as F
+
 from skimage import io
 
 
@@ -22,30 +24,73 @@ dataset = MNIST('image_cla_lr/data', train=False, transform=Transforms.ToTensor(
 
 
 ## MODEL
-model = nn.Sequential(
-  nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
-  nn.ReLU(),
-  nn.AvgPool2d(2,2), # bc, 16, 14, 14 
+def conv_2d(ni, nf, ks=3, stride=1):
+  return nn.Conv2d(ni, nf, kernel_size=ks, stride=stride, padding=ks//2, bias=False)
 
-  nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-  nn.ReLU(),
-  nn.AvgPool2d(2,2), # bc, 32, 7, 7
+def bn_relu_conv(ni, nf):
+  return nn.Sequential(
+    nn.BatchNorm2d(ni),
+    nn.ReLU(inplace=True),
+    conv_2d(ni, nf)
+  )
 
-  nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-  nn.ReLU(),
-  nn.AvgPool2d(2,2), # bc, 64, 3.5, 3.5
+class ResidualBlock(nn.Module):
+  def __init__(self, ni, nf, stride=1):
+    super(ResidualBlock, self).__init__()
 
-  nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-  nn.ReLU(),
-  nn.AvgPool2d(2,2),
+    self.bn = nn.BatchNorm2d(ni)
+    self.conv1 = conv_2d(ni, nf, stride=stride)
+    self.conv2 = bn_relu_conv(nf, nf)
+    self.shortcut = lambda x: x
+    if ni != nf:
+      self.shortcut = conv_2d(ni, nf, ks=1, stride=stride)
+    
+  def forward(self, x):
+    x = F.relu(self.bn(x), inplace=True)
+    r = self.shortcut(x)
+    x = self.conv1(x)
+    x = self.conv2(x) * 0.2
+    return x.add_(r)
 
-  nn.Flatten(),
-  nn.Linear(128, 10)
-)
+def make_group(N, ni, nf, stride):
+  start = ResidualBlock(ni, nf, stride)
+  rest = [ResidualBlock(nf, nf) for j in range(1, N)]
+  return [start] + rest
+
+
+class WideResNet(nn.Module):
+  def __init__(self, start_channel, N, n_classes, groups=3, k=6):
+    super().__init__()
+
+    layers = [conv_2d(1, start_channel)]
+    channels = [start_channel]
+
+    for i in range(groups):
+      channels.append(start_channel*(2**i)*k)
+      stride = 2 if i > 0 else 1
+      layers += make_group(N, channels[i], channels[i+1], stride)
+
+    layers += [
+      nn.BatchNorm2d(channels[-1]),
+      nn.ReLU(inplace=True),
+      nn.AdaptiveAvgPool2d(1),
+      nn.Flatten(),
+      nn.Linear(channels[-1], n_classes)
+    ]
+
+    self.features = nn.Sequential(*layers)
+
+  def forward(self, x): return self.features(x)
+
+
+def wdr22():
+  return WideResNet(start_channel=16, N=3, n_classes=10, groups=3, k=6)
+
+model = wdr22()
 
 
 # Loading the trained model
-model.load_state_dict(torch.load('image_cla_lr/models/model6'))
+model.load_state_dict(torch.load('image_cla_lr/models/model8'))
 
 # Setting the model for evaluation mode
 model.eval()
@@ -53,7 +98,7 @@ model.eval()
 
 
 
-img_name = '8.png'
+img_name = '7.png'
 
 # Importing image
 img1 = io.imread('image_cla_lr/data/images/' + str(img_name), as_gray=True)
@@ -74,6 +119,12 @@ input_img_data = input_img_data.type(torch.float32)
 # Creating the image label
 input_img_label = torch.tensor([int(img_name[0])])
 
+
+# Normalizing the image
+def normalize(img):
+  return img - 0.5 / 0.5
+
+input_img_data = normalize(input_img_data)
 
 
 
